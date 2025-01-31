a0dc7fa56b43392a5382de027809fb186a17dc28
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-31 13:51:34 +0200

    Add assets and update configuration for fonts and images
    Added various image and font assets to the project. Updated `pubspec.yaml` to include the new assets and configured the fonts with their respective weights. Also, created utility files `app_colors.dart`, `app_images.dart`, and `app_styles.dart` to manage colors, image paths, and text styles respectively.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Color(0xff4C4DDC);
+  static const Color secondaryColorTheme = Color(0xffEDEDFC);
+  static const Color blackTheme = Color(0xff101010);
+  static const Color subColorsFirst = Color(0xffC8C8F4);
+  static const Color subColorsSecond = Color(0xffD4D4D8);
+}
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBell
+  /// assets/images/bell.svg
+  static const String imagesBell = "assets/images/bell.svg";
+
+  /// Assets for imagesDoctor1
+  /// assets/images/Doctor1.png
+  static const String imagesDoctor1 = "assets/images/Doctor1.png";
+
+  /// Assets for imagesDoctor2
+  /// assets/images/Doctor2.png
+  static const String imagesDoctor2 = "assets/images/Doctor2.png";
+
+  /// Assets for imagesDoctor3
+  /// assets/images/Doctor3.png
+  static const String imagesDoctor3 = "assets/images/Doctor3.png";
+
+  /// Assets for imagesDoctor4
+  /// assets/images/Doctor4.png
+  static const String imagesDoctor4 = "assets/images/Doctor4.png";
+
+  /// Assets for imagesDoctor5
+  /// assets/images/Doctor5.png
+  static const String imagesDoctor5 = "assets/images/Doctor5.png";
+
+  /// Assets for imagesGroup
+  /// assets/images/group.svg
+  static const String imagesGroup = "assets/images/group.svg";
+
+  /// Assets for imagesInvest
+  /// assets/images/invest.svg
+  static const String imagesInvest = "assets/images/invest.svg";
+
+  /// Assets for imagesMen
+  /// assets/images/men.jpg
+  static const String imagesMen = "assets/images/men.jpg";
+
+  /// Assets for imagesMore
+  /// assets/images/more.svg
+  static const String imagesMore = "assets/images/more.svg";
+
+  /// Assets for imagesReviews
+  /// assets/images/reviews.svg
+  static const String imagesReviews = "assets/images/reviews.svg";
+
+  /// Assets for imagesSearchNormal
+  /// assets/images/search-normal.svg
+  static const String imagesSearchNormal = "assets/images/search-normal.svg";
+
+  /// Assets for imagesStar
+  /// assets/images/star.svg
+  static const String imagesStar = "assets/images/star.svg";
+
+  /// Assets for imagesTexture
+  /// assets/images/Texture.svg
+  static const String imagesTexture = "assets/images/Texture.svg";
+}
+
