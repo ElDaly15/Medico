aaa11c498e2c9bde119939c7c05424fa840cc1c2
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-02-04 12:22:43 +0200

    Move `ContainerOfMainDoctorAppoitnement` to separate file
    Refactor the `appointment_view_body.dart` file by moving the `ContainerOfMainDoctorAppoitnement` widget to a new file named `container_of_main_doctor_appointment.dart`. This change improves code organization and maintainability.

--- lib/featuers/appointment/presentation/views/widgets/appointment_view_body.dart
+++ lib/featuers/appointment/presentation/views/widgets/appointment_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:medico/core/utils/app_images.dart';
-import 'package:medico/core/utils/app_styles.dart';
+import 'package:medico/featuers/appointment/presentation/views/widgets/container_of_main_doctor_appointment.dart';
 import 'package:medico/featuers/appointment/presentation/views/widgets/custom_header.dart';
 
 class AppointmentViewBody extends StatelessWidget {
@@ -27,82 +26,6 @@ class AppointmentViewBody extends StatelessWidget {
           child: ContainerOfMainDoctorAppoitnement(),
         ),
       ],
-    );
-  }
-}
-
-class ContainerOfMainDoctorAppoitnement extends StatelessWidget {
-  const ContainerOfMainDoctorAppoitnement({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 12,
-            offset: const Offset(0, 1),
-          ),
-        ],
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-            child: Image.asset(
-              width: double.infinity,
-              Assets.imagesDoctor6,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              children: [
-                Text('Dr. Jenny Wilson',
-                    style: TextStyles.font24SemiBold(context)),
-                const Spacer(),
-                const Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 25,
-                ),
-                Text('5.0', style: TextStyles.font18SemiBold(context)),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text(
-                  '(322 reviews)',
-                  style: TextStyles.font14SemiBold(context).copyWith(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Text(
-              'Neurologist | Vcare Clinic',
-              style:
-                  TextStyles.font18Medium(context).copyWith(color: Colors.grey),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
     );
   }
 }
